c3a9dde6962afadb6c088be97b0582a3dce87937
thowimmer <thowimmer@pm.me>
2021-12-30 16:35:57 +0100

    Solved second part of Day14.

--- src/main/kotlin/Day14.kt
+++ src/main/kotlin/Day14.kt
@@ -1,6 +1,6 @@
-class Day14(input: List<String>) : Day<Day14.Input, Int>(input) {
+class Day14(input: List<String>) : Day<Day14.Input, Long>(input) {
 
-    class Input(val template: List<String>, val rules: Map<String, Char>)
+    class Input(val templatePairCounts: Map<String, Long>, val templateCharCounts: Map<Char, Long>, val rules: Map<String, Char>)
 
     override fun convertInput(input: List<String>): Input {
         val rules = mutableMapOf<String, Char>()
@@ -8,58 +8,43 @@ class Day14(input: List<String>) : Day<Day14.Input, Int>(input) {
             val (pair, char) = rule.split(" -> ")
             rules[pair] = char.toCharArray()[0]
         }
-        return Input(input[0].toPairs(), rules)
+
+        val template = input[0]
+        val charCounts = template.toCharArray().groupBy { it }.mapValues { it.value.size.toLong() }
+        val pairCounts = mutableMapOf<String, Long>()
+        template
+            .mapIndexed { index, char -> if(index < template.length - 1) "$char${template[index+1]}" else "" }
+            .filterNot { it == "" }
+            .forEach {pairCounts.increaseCounter(it, 1L)}
+
+        return Input(pairCounts, charCounts, rules)
     }
 
-    override fun runPart1(input: Input): Int {
-        var polymerPairs = input.template.toMutableList()
-        for(i in 1..10){
-            var newPolymerPairs = polymerPairs
-            var insertions = 0
-            for((index, pair) in polymerPairs.withIndex()){
-                val insertionChar = input.rules[pair]
-                if(insertionChar != null){
-                    val insertionIndex = insertions + index
-                    newPolymerPairs = (
-                            newPolymerPairs.subList(0, insertionIndex)
-                            + listOf("${pair[0]}$insertionChar")
-                            + listOf("$insertionChar${pair[1]}")
-                            + newPolymerPairs.subList(insertionIndex+1, newPolymerPairs.size)
-                    ).toMutableList()
+    override fun runPart1(input: Input): Long = solve(input, 10)
 
-                    insertions++
-                }
+    override fun runPart2(input: Input): Long = solve(input, 40)
+
+    private fun <T> MutableMap<T, Long>.increaseCounter(key: T, inc: Long){
+        val currentValue = this.getOrPut(key) { 0 }
+        this[key] = currentValue + inc
+    }
+
+    private fun solve(input: Input, times: Int) : Long {
+        var pairs = input.templatePairCounts
+        val charCounts = input.templateCharCounts.toMutableMap()
+
+        for(i in 1..times){
+            val newPairs = mutableMapOf<String, Long>()
+            for((pair, value) in pairs){
+                val newChar = input.rules[pair]!!
+                newPairs.increaseCounter("${pair[0]}$newChar", value)
+                newPairs.increaseCounter("$newChar${pair[1]}", value)
+                charCounts.increaseCounter(newChar, value)
             }
-            polymerPairs = newPolymerPairs
+            pairs = newPairs
         }
 
-        val quantities = polymerPairs.mergePairs().groupBy { it }.values.map { it.size }.sorted()
+        val quantities = charCounts.values.sorted()
         return quantities.last() - quantities.first()
-    }
-
-    private fun String.toPairs() : List<String> {
-        val pairs = mutableListOf<String>()
-        var pairIndex = 0
-        while(pairIndex <= this.length - 2){
-            pairs.add("${this[pairIndex]}${this[pairIndex+1]}")
-            pairIndex++
-        }
-
-        return pairs
-    }
-
-    private fun List<String>.mergePairs() : String {
-        val sb = StringBuilder()
-        for((index, pair) in this.withIndex()){
-            sb.append(pair[0])
-            if(index == this.size-1){
-                sb.append(pair[1])
-            }
-        }
-        return sb.toString()
-    }
-
-    override fun runPart2(input: Input): Int {
-        return -1
     }
 }
--- src/test/kotlin/Day14Test.kt
+++ src/test/kotlin/Day14Test.kt
@@ -14,6 +14,6 @@ class Day14Test {
     fun shouldPassPart2(){
         val input = InputLoader().loadInput(14)
         val day14 = Day14(input)
-        assertEquals(-1, day14.runPart2())
+        assertEquals(4704817645083, day14.runPart2())
     }
 }
